901b55a6f54cee9a8c36cb93dd5a9c40c53d46a9
Tran Phuoc Tin <tptin.76@gmail.com>
2024-02-03 21:34:24 +0700

    udpate

--- Source/Engine/CEngine.cpp
+++ Source/Engine/CEngine.cpp
@@ -1,12 +1,15 @@
 #include "CEngine.h"
+#include <LoggerDefines.h>
 
 CEngine* CEngine::s_instance = nullptr;
 CEngine::CEngine()
 {
+    _DEBUG("%s", __FUNCTION__);
 }
 
 CEngine::~CEngine()
 {
+    _DEBUG("%s", __FUNCTION__);
 }
 
 CEngine *CEngine::instance()
