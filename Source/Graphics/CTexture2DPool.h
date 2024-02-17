3fb2101779415891a3c40f07cbc483a15c9ba0b8
Tran Phuoc Tin <tptin.76@gmail.com>
2024-02-17 17:29:08 +0700

    Implementing Graphics

--- Source/Graphics/CTexture2DPool.h
+++ Source/Graphics/CTexture2DPool.h
@@ -13,13 +13,15 @@ private:
     std::unordered_map<const char*, SDL_Texture*> m_textures;
 
 public:
+    CTexture2DPool(CTexture2DPool &&) = delete;
+    CTexture2DPool &operator=(CTexture2DPool &&) = delete;
     CTexture2DPool(CTexture2DPool &other) = delete;
     void operator=(const CTexture2DPool &) = delete;
     ~CTexture2DPool();
-    static CTexture2DPool* instance();
+    static CTexture2DPool *instance();
 
-    bool load(const char* id, const char* src);
-    void remove(const char* id);
+    bool load(const char *id, const char *src);
+    void remove(const char *id);
 };
 
 #endif // CTEXTURE2DPOOL_H
