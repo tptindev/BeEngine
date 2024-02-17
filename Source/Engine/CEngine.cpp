718c320a2c1adfa40cdd658213402bfcd9107e38
Tran Phuoc Tin <tptin.76@gmail.com>
2024-02-17 18:03:51 +0700

    Implementing Graphics

--- Source/Engine/CEngine.cpp
+++ Source/Engine/CEngine.cpp
@@ -1,10 +1,12 @@
 #include "CEngine.h"
 #include <LoggerDefines.h>
 #include <CMemoryManager.h>
+#include <SDL_timer.h>
 
 CEngine* CEngine::s_instance = nullptr;
+bool CEngine::s_running = false;
 CEngine::CEngine(CObject *parent)
-    :CObject(parent), m_running(true)
+    :CObject(parent)
 {
     _DEBUG("%s", __FUNCTION__);
 
@@ -22,11 +24,45 @@ CEngine *CEngine::instance()
 
 bool CEngine::initialize()
 {
+    s_running = true;
     return true;
 }
 
 void CEngine::loop()
 {
+    const int fps = 60;
+    const int frameDelay = 1000 / fps;
+
+    // time between 2 frames
+    float deltaTime = 0.0f;
+
+    // time at start
+    Uint32 lastTime = SDL_GetTicks();
+
+    while (s_running)
+    {
+        // current time
+        Uint32 currentTime = SDL_GetTicks();
+
+        // calculate deltatime
+        deltaTime = (float)(currentTime - lastTime) / 1000.0f;
+
+        // update lastTime
+        lastTime = currentTime;
+
+        // [game logic]
+        {
+//            handle_events();
+//            update(deltaTime);
+//            render();
+        }
+        // [game logic]
+
+        if (deltaTime < frameDelay)
+        {
+            SDL_Delay(frameDelay - deltaTime);
+        }
+    }
 }
 
 void CEngine::clean()
--- Source/Engine/CEngine.h
+++ Source/Engine/CEngine.h
@@ -8,16 +8,18 @@ class CEngine: public CObject
 {
 private:
     static CEngine* s_instance;
+    static bool s_running;
     explicit CEngine(CObject* parent = nullptr);
     ~CEngine();
 
-    bool m_running;
     CTimer m_timer;
 
 public:
+    CEngine(CEngine &&) = delete;
+    CEngine &operator=(CEngine &&) = delete;
     CEngine(CEngine &other) = delete;
     void operator=(const CEngine &) = delete;
-    static CEngine* instance();
+    static CEngine *instance();
 
     bool initialize();
     void loop();
