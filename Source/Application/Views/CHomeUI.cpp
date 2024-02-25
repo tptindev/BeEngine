cf0b4adf6f69a6b34f31fc081f11483da0d3a379
Tran Phuoc Tin <tptin.76@gmail.com>
2024-02-26 00:46:57 +0700

    Implementing Button

--- Source/Application/Views/CHomeUI.cpp
+++ Source/Application/Views/CHomeUI.cpp
@@ -38,7 +38,7 @@ CHomeUI::CHomeUI(CApplication* app): CLayer(app)
         m_quit_btn->setWidth(50);
         m_quit_btn->setHeight(25);
         m_quit_btn->setX(app->window()->width() - m_quit_btn->width());
-        m_quit_btn->setY(app->window()->height() - m_quit_btn->height());
+        m_quit_btn->setY(0);
         m_quit_btn->stateChanged().connect([&](CButton::E_BTN_STATE state){
             if(state == CButton::E_BTN_STATE::CLICKED)
             {
@@ -58,11 +58,11 @@ CHomeUI::CHomeUI(CApplication* app): CLayer(app)
 
         // options btn
         m_options_btn->setX(m_start_btn->x());
-        m_options_btn->setY(m_start_btn->y() + m_start_btn->height() + spacing);
+        m_options_btn->setY(m_start_btn->y() + m_start_btn->height() + 25);
 
         // quit btn
         m_quit_btn->setX(w - m_quit_btn->width());
-        m_quit_btn->setY(h - m_quit_btn->height());
+        m_quit_btn->setY(0);
 
     });
 }
